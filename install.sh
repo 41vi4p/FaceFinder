6e80569a0920c84723ee879a4dd9a843fc63f558
David Porathur <93052495+41vi4p@users.noreply.github.com>
2024-12-05 13:42:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt install python3-pip
 sudo apt install python3
 sudo apt install cmake
-pip install cmake
+pip install cmake --break-system-packages
 pip install -r requirements.txt --break-system-packages
