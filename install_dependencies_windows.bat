385085b88bccf143aa013c5d4b565b860fc4e2c3
41vi4p <dav7.digi@yahoo.com>
2025-01-17 09:43:21 +0530

    uncomment_install

--- install_dependencies_windows.bat
+++ install_dependencies_windows.bat
@@ -4,23 +4,20 @@ git clone https://github.com/41vi4p/FaceFinder.git
 cd FaceFinder
 
 echo Installing Miniconda3...
-@REM win_installer_files\Miniconda3-latest-Windows-x86_64.exe
-
-@REM :: Silent installation of Miniconda3
-@REM %OUTPUT% /InstallationType=JustMe /RegisterPython=0 /AddToPath=1 /S /D=%USERPROFILE%\Miniconda3
+win_installer_files\Miniconda3-latest-Windows-x86_64.exe
 
 echo Installation complete.
 
 @REM CMake install
 echo Installing CMake... Follow the instructions on the Window.
 timeout /t 3
-@REM win_installer_files\cmake-3.31.4-windows-x86_64.msi
+win_installer_files\cmake-3.31.4-windows-x86_64.msi
 
 @REM Install Visual Studio Community
 echo Installing Visual Studio Community... Follow the instructions on the Window.
 echo Install the 'Desktop Development with C++' Package on the Open Window
 timeout /t 5
-@REM win_installer_files\VisualStudioSetup.exe
+win_installer_files\VisualStudioSetup.exe
 echo Done Installing 'Desktop Development with C++' from Visual Studio Community
 
 
